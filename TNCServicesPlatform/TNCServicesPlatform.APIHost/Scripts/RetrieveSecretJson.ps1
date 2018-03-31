d7de21247aa14cb8ced556f339a622b1b60cdd75
Zhimin Yao <yao.zhimin@hotmail.com>
2018-03-30 20:50:29 -0700

    Add powershell to update and retrieve secret.json based on Azure login

---
+++ TNCServicesPlatform/TNCServicesPlatform.APIHost/Scripts/RetrieveSecretJson.ps1
@@ -0,0 +1,6 @@
+# read secret.json
+$tncSubscriptionId = '753a7f10-1bc9-4ac4-bdb2-fac72e44d41d'
+Login-AzureRmAccount
+Set-AzureRmContext -SubscriptionId $tncSubscriptionId
+$secretjsoncontent = (get-azurekeyvaultsecret -vaultName "TNCServicesKeyVault" -name "secretjsonvalue").SecretValueText
+$secretjsoncontent | Set-Content "..\secret.json"
---
+++ TNCServicesPlatform/TNCServicesPlatform.APIHost/Scripts/UpdateSecretJson.ps1
@@ -0,0 +1,7 @@
+# read secret.json
+$tncSubscriptionId = '753a7f10-1bc9-4ac4-bdb2-fac72e44d41d'
+Login-AzureRmAccount
+Set-AzureRmContext -SubscriptionId $tncSubscriptionId
+$SecretJsonContent = [IO.File]::ReadAllText("..\secret.json")
+$secretvalue = ConvertTo-SecureString $SecretJsonContent -AsPlainText -Force
+$secret = Set-AzureKeyVaultSecret -VaultName 'TNCServicesKeyVault' -Name 'secretjsonvalue' -SecretValue $secretvalue
